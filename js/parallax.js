f14b64a2e23453ec2ff1d62bc61c7084a350912c
Hemanth Mukesh <hmukesh@outlook.com>
2019-07-25 18:36:54 -0500

    fixed parallax on refreshing website

--- js/parallax.js
+++ js/parallax.js
@@ -1,3 +1,6 @@
+var scrollfir = $(window).scrollTop();
+$('.parallax').css('background-position', 'center '+(scrollfir*0.35)+'px')
+
 $(window).scroll(function() {
     var scrollPos = $(window).scrollTop();
     $('.parallax').css('background-position', 'center '+(scrollPos*0.35)+'px')
